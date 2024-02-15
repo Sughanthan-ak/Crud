932362030e7d3e17bacbf5ea5f12d8670c42af6d
Balamurugane <A022447486@hepl.local>
2024-02-15 15:52:36 +0530

    updated table

--- records/src/App.js
+++ records/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+ 
+const App = () => {
+  const [users, setUsers] = useState([]);
+ 
+  useEffect(() => {
+    getUsers();
+  }, []);
+ 
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark bg-light justify-content-center">
+        <h2 className="text-primary">Employee Details</h2>
+      </nav>
+      <div className="container">
+        <table className="table" border="2">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Location</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.address.geo.lat},{user.address.geo.lng}</td>
+                <td><a href={user.website}>Check my site here</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
-
+};
+ 
 export default App;
+ 
