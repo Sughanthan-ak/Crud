f0ff3bd22687f0fe1b7966fe581de8d26ada7cae
Sughanthan <A022417927@hepl.local>
2024-02-15 15:49:03 +0530

    Readed

--- records/src/App.js
+++ records/src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
@@ -6,11 +7,9 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
-       
-        const response = await fetch('https://api.example.com/data');
+        const response = await axios.get('https://api.example.com/data');
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
@@ -22,18 +21,50 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Read Data</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>
-            {item.name} - {item.description}
-          </li>
-        ))}
-      </ul>
+    <div id="table">
+      <h1>Table3</h1>
+      <table class="t">
+        <thead>
+          <tr>
+            <th>01</th>
+            <th>Elakiya</th>
+            <th>Developer</th>
+          </tr>
+          <tr>
+            <th>02</th>
+            <th>bala</th>
+            <th>Developer</th>
+          </tr>
+          <tr>
+            <th>03</th>
+            <th>Malavan</th>
+            <th>Developer</th>
+          </tr>
+          <tr>
+            <th>04</th>
+            <th>srinath</th>
+            <th>Developer</th>
+          </tr>
+          <tr>
+            <th>05</th>
+            <th>sughanthan</th>
+            <th>Developer</th>
+          </tr>
+          
+          
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 
 export default App;
