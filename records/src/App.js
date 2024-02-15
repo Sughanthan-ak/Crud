d4d9a6a7225ef52fff34dac7fa8b9a48274b0ef1
MMalavan <159991875+MMalavan@users.noreply.github.com>
2024-02-15 15:43:50 +0530

    Updated

--- records/src/App.js
+++ records/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import {EmpTable} from  './compenents/emp-table';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+  
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      console.log(response);
+      setUsers(response?.data);
+    });
+  }, []);
+
+    return(
+      <div>
+      <nav className="navbar navbar-dark bg-light justify-content-center">
+        <h2 className="text-primary">Employee Details</h2>
+      </nav>
+      <div className="container">
+        <table className="table" border="1">
+          <thead>
+            <tr>
+            <th>Id</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Location</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+              <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.address.geo.lat},{user.address.geo.lng}</td>
+                <td><a href={user.website}>Check my site here </a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
